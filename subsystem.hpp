eaee7cd799acce41712aa445e9f15d2742130f27
GHawk1124 <garrett1124@icloud.com>
2021-06-22 07:28:35 -0700

    Added subsystem class.

--- subsystem.hpp
+++ subsystem.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "stateMachine.hpp"
+
+namespace apex {
+class subsystem : public StateMachine {
+public:
+  void init();
+
+  void execute();
+
+  void shutdown();
+};
+} // namespace apex
